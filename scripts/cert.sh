d59710eba41b28d6d47172cc88e3b6181dc62ec7
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2022-07-25 13:33:27 +0430

    fix: dhparm

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -6,6 +6,7 @@ rm -rf nginx/.defaults/cert/*.pem
 cd nginx/.defaults/cert
 cfssl gencert -initca config.root.json | cfssljson -bare ca
 cfssl gencert -ca ca.pem -ca-key ca-key.pem -config config.json -profile=server config.server.json | cfssljson -bare server
+curl https://ssl-config.mozilla.org/ffdhe2048.txt > dhparam.pem
 
 mv ca.pem chain.pem
 mv server.pem cert.pem

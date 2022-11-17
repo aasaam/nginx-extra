417bb694dc1e7a266cd15c5cc2bb6cc84b28508b
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2022-11-17 21:15:22 +0330

    fix: nginx var envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 cp /.defaults/ssl/profile.${ASM_NGX_EXTRA_SSL_PROFILE}.conf /.defaults/ssl/profile.runtime.conf
-envsubst < /template/nginx.conf > /etc/nginx/nginx.conf
+envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < /template/nginx.conf > /etc/nginx/nginx.conf
 exec "${@}"

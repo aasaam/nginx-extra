ef4ef5411c108bb24949c4f2f2ecc824dd71f2e9
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2022-12-09 19:18:51 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 set -e
 cp /.defaults/ssl/profile.${ASM_NGX_EXTRA_SSL_PROFILE}.conf /.defaults/ssl/profile.runtime.conf
+echo "SSL Profile: ${ASM_NGX_EXTRA_SSL_PROFILE} enabled by default."
 envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < /template/nginx.conf > /etc/nginx/nginx.conf
 exec "${@}"
